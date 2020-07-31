9501628a8b1eea66dce852f1b7615a2fbfd3ee8a
Donnie <donnieashok@gmail.com>
2020-07-31 18:21:15 +0200

    Save with protobuffers

--- test.py
+++ test.py
@@ -1,18 +1,5 @@
-import pickle
 import pandas as pd
-from tensorflow.python.keras.layers import deserialize
-from tensorflow.python.keras.saving import saving_utils
-
-def unpack(model, training_config, weights):
-    restored_model = deserialize(model)
-    if training_config is not None:
-        restored_model.compile(
-            **saving_utils.compile_args_from_training_config(
-                training_config
-            )
-        )
-    restored_model.set_weights(weights)
-    return restored_model
+from tensorflow.keras.models import load_model
 
 test_file = "data/test.csv"
 raw_data_test = pd.read_csv(test_file)
@@ -22,7 +9,7 @@ text_x = raw_data_test.values.astype('float32')
 text_x = text_x.reshape(text_x.shape[0], 28, 28,1)
 text_x.shape
 
-model = pickle.load(open('output/finalized_model.sav', 'rb'))
+model = load_model('output/digit')
 predictions = model.predict_classes(text_x, verbose=0)
 
 submissions=pd.DataFrame({"ImageId": list(range(1,len(predictions)+1)),
--- train.py
+++ train.py
@@ -1,34 +1,8 @@
 import numpy as np
 import pandas as pd
-import pickle
 from tensorflow import keras
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, Flatten, Conv2D
-from tensorflow.python.keras.layers import deserialize, serialize
-from tensorflow.python.keras.saving import saving_utils
-
-def unpack(model, training_config, weights):
-    restored_model = deserialize(model)
-    if training_config is not None:
-        restored_model.compile(
-            **saving_utils.compile_args_from_training_config(
-                training_config
-            )
-        )
-    restored_model.set_weights(weights)
-    return restored_model
-
-def make_keras_picklable():
-
-    def __reduce__(self):
-        model_metadata = saving_utils.model_metadata(self)
-        training_config = model_metadata.get("training_config", None)
-        model = serialize(self)
-        weights = self.get_weights()
-        return (unpack, (model, training_config, weights))
-
-    cls = Model
-    cls.__reduce__ = __reduce__
 
 img_rows, img_cols = 28, 28
 num_classes = 10
@@ -61,10 +35,16 @@ model.compile(loss=keras.losses.categorical_crossentropy,
               metrics=['accuracy'])
 model.fit(x, y,
           batch_size=128,
-          epochs=4,
+          epochs=3,
           validation_split = 0.2)
 
-# Run the function
-make_keras_picklable()
-with open('output/finalized_model.sav', 'wb') as f:
-    pickle.dump(model, f)
+model.save('output/digit')
+
+print('input_node_names:')
+for t in model.inputs:
+    print(t.name)
+
+print('output_node_names:')
+for t in model.outputs:
+    print(t.name)
+
